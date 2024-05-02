f1f8a1f231b677efd3a97b529371ea007424a461
techwithradhika <radhikam406@gmail.com>
2024-05-02 22:26:35 +0530

    Remove notes_project from Git

--- notes_project/notes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NoteListCreateAPIView, NoteRetrieveUpdateDestroyAPIView
-
-urlpatterns = [
-    path('api/notes/', NoteListCreateAPIView.as_view(), name='note-list-create'),
-    path('api/notes/<int:pk>/', NoteRetrieveUpdateDestroyAPIView.as_view(), name='note-detail'),
-]
